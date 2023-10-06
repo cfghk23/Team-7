99ce03c9e4201c57ce961396cce05d3edd892a59
soniazzz <soniaz05@connect.hku.hk>
2023-10-06 12:19:58 +0800

    test

---
+++ sim-game/src/Page/Login.jsx
@@ -0,0 +1,142 @@
+import * as React from 'react'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import Link from '@mui/material/Link'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log(JSON.stringify(formData))
+    const response = await fetch(
+      'http://127.0.0.1:8000/bias_test/api/signup/',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      }
+    )
+    const data = await response.json()
+    if (response.ok) {
+      // navigate('/mainPage')
+      // if (data.error && data.error === 'Username already exists.') {
+      //   alert('Username already exists. Please choose another one.')
+      // } else {
+      //   navigate('/login')
+      // }
+    } else {
+      alert('Error signing up. Please try again.')
+    }
+  }
+  return (
+    <div
+      style={{
+        position: 'relative',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          height: '100%',
+          width: '100%',
+          backgroundImage: `url(https://media.istockphoto.com/id/1340947061/zh/%E5%90%91%E9%87%8F/fairytale-kingdom-map-medieval-village-background-vector-fairy-tale-castle-infographic-elements.jpg?s=1024x1024&w=is&k=20&c=Vh6z1ZmqCZZEFyMbqYz9TJ_DJIwSUSsJE3x3k6aQ5K8=)`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          opacity: 0.5,
+          zIndex: -1,
+        }}
+      />
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Box
+          sx={{
+            // marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, backgroundColor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography
+            component='h1'
+            variant='h5'
+            fontStyle={{ color: 'black' }}
+          >
+            Sign in
+          </Typography>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='username'
+              label='Username'
+              name='username'
+              autoComplete='username'
+              onChange={handleChange}
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              id='password'
+              autoComplete='current-password'
+              onChange={handleChange}
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href='/signup' variant='body2'>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+  )
+}
