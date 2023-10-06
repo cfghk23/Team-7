0e46efc245c7f80fa0f723da69795d9415dfaa03
Alan Ngo <u3556436@hku.hk>
2023-10-06 16:31:11 +0800

    update database

--- Backend/database.py
+++ Backend/database.py
@@ -28,6 +28,12 @@ class StudentCourseBridge(db.Model):
     progress = db.Column(db.Integer)
     status = db.Column(db.String, nullable=False)
 
+class Course(db.Model):
+    course_id = db.Column(db.Integer, primary_key=True)
+    course_title = db.Column(db.String, unique=True, nullable=False)
+    course_desc = db.Column(db.String, nullable=False)
+    course_img = db.Column(db.String, nullable=True)
+
 with app.app_context():
     db.create_all()
 
@@ -55,6 +61,21 @@ with app.app_context():
     #
     # db.session.commit()
 
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    # Query the database to check if the student exists
+    student = Student.query.filter_by(email=email).first()
+
+    if student and student.password == password:
+        # Authentication successful
+        return jsonify({'message': 'Login successful'})
+    else:
+        # Authentication failed
+        return jsonify({'message': 'Invalid email or password'})
+
 
 @app.route("/studentinfo/<int:sid>")
 def getStudentInfo(sid):
@@ -68,6 +89,31 @@ def getStudentInfo(sid):
   print(return_json)
 
   return jsonify(return_json)
+
+@app.route("/studentprogress/<int:sid>")
+def getStudentProgress(sid):
+    studentProgress = db.get_or_404(StudentCourseBridge, sid) 
+
+    return_json = studentProgress
+    print(return_json)
+
+    return jsonify(return_json)
+
+
+# dashboard for score
+@app.route('/average_score/<int:course_id>')
+def average_score(course_id):
+    course = Course.query.get(course_id)
+
+    if not course:
+        return jsonify({'message': 'Course not found'})
+
+    scores = [scb.score for scb in course.students if scb.score is not None]
+    if len(scores) == 0:
+        return jsonify({'message': 'No scores available for this course'})
+
+    average = sum(scores) / len(scores)
+    return jsonify({'average_score': average})
 
 if __name__ == '__main__':
     app.run(debug=True)
